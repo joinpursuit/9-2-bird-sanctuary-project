5ed5b5111c769fb5515b4d7ed1329f08e772a6e1
i-guerrero <isaacguerrero@pursuit.org>
2022-10-23 13:08:00 -0400

    pass first test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-function App () {
+import BirdCard from "./components/BirdCard";
+import birdData from "./data/birds";
+import { useState } from "react";
+import Cart from "./components/Cart";
+
+function App() {
+  const [birds, setBirds] = useState(birdData);
+  const [cart, setCart] = useState("");
+
+  function addToCart(bird) {
+    setCart({
+      ...cart,
+      bird.name,
+    })
+    return <ol>{bird.name}</ol>;
+  }
+
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <Cart addToCart={addToCart} />
+      {birdData.map((bird) => {
+        return <BirdCard bird={bird} addToCart={addToCart} />;
+      })}
     </div>
   );
-};
+}
 
 export default App;

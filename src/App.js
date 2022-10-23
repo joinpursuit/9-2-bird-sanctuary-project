3a6977928a0c8347418962b44354cddb1086c3e3
“danielsolaquegit config --global user.email “danielsolaque@pursuit.org <example@email.com>
2022-10-23 12:53:40 -0400

    not completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-function App () {
+import React from "react";
+import BirdCard from "./Components/BirdCard";
+
+function App() {
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <h1>My project</h1>
     </div>
   );
 };
 
 export default App;
+
+
+//Metodologia de trabajo 1er proyecto react:
+
+//1. Crear componentes hijos de App: bird cards, checkout, cart.
+////1.1 bird cards sera el padre de bird card.
+
+//2.Crear un estado(1) en 'App" padre que actualice su valor con la informacion de "bird card" elegida por el usuario.
+////**{desde App padre se enviara como una prop la funcion transformadora del estado para que se ejecute con la informacion elegida por el usuario y/o ejecutadam por nuestro componente}
+
+//3.Enviar como prop la informacion del estado de "bird cards" para ejecutar el componente "cart". 
+////3.1 Crear un estado(2) en 'App" padre que actualice su valor con la informacion de "Cart" elegida por el usuario.
+
+//4.Enviar como prop la informacion del estado de "cart" para ejecutar el componente "checkout". 
+
+////Podran necesitarse mas estados dentro de los componentes.
+
---
+++ src/Components/BirdCard.js
@@ -0,0 +1,10 @@
+export default function BirdCard() {
+    return (
+        <section className="Card">
+            <h2>BirdName</h2>
+            <img src="https://img.freepik.com/vector-premium/imagen-perfil-avatar-hombre-ilustracion-vectorial_268834-541.jpg" alt="user img" className='img'>Isert Image</img>
+            <input type="button" value="Click me">ADOPT</input>
+        </section>
+        
+    );
+}

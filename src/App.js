7f3875002036acb5656af666afcf941be8c04abc
i-guerrero <isaacguerrero@pursuit.org>
2022-11-19 21:30:53 -0500

    passes checkout and reset

--- src/App.js
+++ src/App.js
@@ -2,11 +2,32 @@ import BirdCard from "./components/BirdCard";
 import birdData from "./data/birds";
 import { useState } from "react";
 import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
 
 function App() {
   const [birds, setBirds] = useState(birdData);
   const [cart, setCart] = useState([]);
   const [total, setTotal] = useState(0);
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    zip: "",
+    email: "",
+  });
+
+  function handleTextChange() {}
+
+  function handleSubmit() {
+    alert("You have adopted birds. Thank you!");
+    setCart([]);
+    setTotal(0);
+    setUser({
+      firstName: "",
+      lastName: "",
+      zip: "",
+      email: "",
+    });
+  }
 
   function handleBirdCardClick(bird) {
     setCart([...cart, bird]);
@@ -16,6 +37,11 @@ function App() {
   return (
     <div>
       <Cart cart={cart} total={total} />
+      <Checkout
+        user={user}
+        handleTextChange={handleTextChange}
+        handleSubmit={handleSubmit}
+      />
       <div className="birds">
         {birdData.map((bird) => {
           return (
--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,9 +1,40 @@
+import userEvent from '@testing-library/user-event'
 import React from 'react'
 
-export default function checkout() {
+export default function checkout({ user, handleTextChange, handleSubmit }) {
   return (
-    <div>
-      
+    <div className='Checkout'>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="firstName">First Name:</label>
+        <input 
+          type="text"
+          value={user.firstName}
+          onChange={handleTextChange}
+          id="firstName"
+        />
+        <label htmlFor="lastName">Last Name:</label>
+        <input 
+          type="text"
+          value={user.lastName}
+          onChange={handleTextChange}
+          id="lastName"
+        />
+        <label htmlFor="email">Email:</label>
+        <input 
+          type="email"
+          value={user.email}
+          onChange={handleTextChange}
+          id="email"
+        />
+        <label htmlFor="zip">Zip Code:</label>
+        <input 
+          type="text"
+          value={user.zip}
+          onChange={handleTextChange}
+          id="zip"
+        />
+        <input type="submit" />
+      </form>
     </div>
   )
 }
